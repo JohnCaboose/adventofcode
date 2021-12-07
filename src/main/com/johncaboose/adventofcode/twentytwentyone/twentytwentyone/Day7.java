d7ce8add0f6abe353a0c9c6eea3b8854b85a69be
JohnCaboose <johncaboose@live.com>
2021-12-07 21:19:43 +0100

    Solved day 7.

---
+++ src/main/com/johncaboose/adventofcode/twentytwentyone/twentytwentyone/Day7.java
@@ -0,0 +1,54 @@
+package com.johncaboose.adventofcode.twentytwentyone.twentytwentyone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.johncaboose.adventofcode.twentytwentyone.twentytwentyone.Utils.populateListWithCommaSeparatedLongs;
+
+public class Day7 implements ISolvableDay {
+
+
+    @Override
+    public long partOneSolver(String input) {
+        List<Long> initialPositions = populateListWithCommaSeparatedLongs(input);
+
+        return calculateMinimumFuelSpent(initialPositions, false);
+    }
+
+    @Override
+    public long partTwoSolver(String input) {
+        List<Long> initialPositions = populateListWithCommaSeparatedLongs(input);
+
+        return calculateMinimumFuelSpent(initialPositions, true);
+    }
+
+    private long calculateMinimumFuelSpent(List<Long> initialPositions, boolean fuelCostIncreasesWithEachStep) {
+        long minimumFuelSpent = Long.MAX_VALUE;
+        Long maximumInitialPosition = initialPositions.stream().max(Long::compareTo).get();
+        for (long i = 0; i < maximumInitialPosition; i++) {
+            long fuelSpent = fuelSpentToMoveTo(i, new ArrayList<>(initialPositions), fuelCostIncreasesWithEachStep);
+            if (fuelSpent < minimumFuelSpent) {
+                minimumFuelSpent = fuelSpent;
+            }
+        }
+
+        return minimumFuelSpent;
+    }
+
+    private static long fuelSpentToMoveTo(final long finalPosition, List<Long> initialPositions) {
+        return fuelSpentToMoveTo(finalPosition, initialPositions, false);
+    }
+
+    private static long fuelSpentToMoveTo(final long finalPosition, List<Long> initialPositions, boolean fuelCostIncreasesWithEachStep) {
+        long fuelSpent = 0;
+        for (long initialPosition : initialPositions) {
+            long distance = Math.abs(initialPosition - finalPosition);
+            fuelSpent += fuelCostIncreasesWithEachStep ? integerSum(distance) : distance;
+        }
+        return fuelSpent;
+    }
+
+    private static long integerSum(long lastValue) {
+        return (lastValue * (lastValue + 1)) / 2;
+    }
+}
