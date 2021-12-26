8b755fe1ea9631ee053725925dab4396737f4406
JohnCaboose <johncaboose@live.com>
2021-12-26 12:26:02 +0100

    Prepared Day 19 and 20.

---
+++ src/main/com/johncaboose/adventofcode/twentytwentyone/days/Day19.java
@@ -0,0 +1,13 @@
+package com.johncaboose.adventofcode.twentytwentyone.days;
+
+public class Day19 implements ISolvableDay {
+    @Override
+    public long partOneSolver(String input) {
+        return 0;
+    }
+
+    @Override
+    public long partTwoSolver(String input) {
+        return 0;
+    }
+}
