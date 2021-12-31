e8527ee65fc1884ecbc7832b41903eec352b4975
JohnCaboose <johncaboose@live.com>
2021-12-31 20:31:28 +0100

    Removed unused main class.

--- src/main/com/johncaboose/adventofcode/Main.java
+++
@@ -1,8 +0,0 @@
-package com.johncaboose.adventofcode;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("This does nothing. Please run unit tests instead.");
-    }
-}
