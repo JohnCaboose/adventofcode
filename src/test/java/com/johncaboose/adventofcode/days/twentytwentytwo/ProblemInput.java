9a7475a784ed50953baba25e6acdcc55a140f04b
JohnCaboose <johncaboose@live.com>
2022-12-10 20:00:31 +0100

    Solved Day 10, but it still has a few pixels off.

--- src/test/java/com/johncaboose/adventofcode/days/twentytwentytwo/ProblemInput.java
+++ src/test/java/com/johncaboose/adventofcode/days/twentytwentytwo/ProblemInput.java
@@ -9824,4 +9824,290 @@ class ProblemInput {
             L 1
             U 1
             D 17""";
+    public static final String DAY_10_EXAMPLE = """
+            addx 15
+            addx -11
+            addx 6
+            addx -3
+            addx 5
+            addx -1
+            addx -8
+            addx 13
+            addx 4
+            noop
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx -35
+            addx 1
+            addx 24
+            addx -19
+            addx 1
+            addx 16
+            addx -11
+            noop
+            noop
+            addx 21
+            addx -15
+            noop
+            noop
+            addx -3
+            addx 9
+            addx 1
+            addx -3
+            addx 8
+            addx 1
+            addx 5
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx -36
+            noop
+            addx 1
+            addx 7
+            noop
+            noop
+            noop
+            addx 2
+            addx 6
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx 1
+            noop
+            noop
+            addx 7
+            addx 1
+            noop
+            addx -13
+            addx 13
+            addx 7
+            noop
+            addx 1
+            addx -33
+            noop
+            noop
+            noop
+            addx 2
+            noop
+            noop
+            noop
+            addx 8
+            noop
+            addx -1
+            addx 2
+            addx 1
+            noop
+            addx 17
+            addx -9
+            addx 1
+            addx 1
+            addx -3
+            addx 11
+            noop
+            noop
+            addx 1
+            noop
+            addx 1
+            noop
+            noop
+            addx -13
+            addx -19
+            addx 1
+            addx 3
+            addx 26
+            addx -30
+            addx 12
+            addx -1
+            addx 3
+            addx 1
+            noop
+            noop
+            noop
+            addx -9
+            addx 18
+            addx 1
+            addx 2
+            noop
+            noop
+            addx 9
+            noop
+            noop
+            noop
+            addx -1
+            addx 2
+            addx -37
+            addx 1
+            addx 3
+            noop
+            addx 15
+            addx -21
+            addx 22
+            addx -6
+            addx 1
+            noop
+            addx 2
+            addx 1
+            noop
+            addx -10
+            noop
+            noop
+            addx 20
+            addx 1
+            addx 2
+            addx 2
+            addx -6
+            addx -11
+            noop
+            noop
+            noop""";
+    public static final String DAY_10_INPUT = """
+            noop
+            noop
+            addx 5
+            addx 29
+            addx -28
+            addx 5
+            addx -1
+            noop
+            noop
+            addx 5
+            addx 12
+            addx -6
+            noop
+            addx 4
+            addx -1
+            addx 1
+            addx 5
+            addx -31
+            addx 32
+            addx 4
+            addx 1
+            noop
+            addx -38
+            addx 5
+            addx 2
+            addx 3
+            addx -2
+            addx 2
+            noop
+            addx 3
+            addx 2
+            addx 5
+            addx 2
+            addx 3
+            noop
+            addx 2
+            addx 3
+            noop
+            addx 2
+            addx -32
+            addx 33
+            addx -20
+            addx 27
+            addx -39
+            addx 1
+            noop
+            addx 5
+            addx 3
+            noop
+            addx 2
+            addx 5
+            noop
+            noop
+            addx -2
+            addx 5
+            addx 2
+            addx -16
+            addx 21
+            addx -1
+            addx 1
+            noop
+            addx 3
+            addx 5
+            addx -22
+            addx 26
+            addx -39
+            noop
+            addx 5
+            addx -2
+            addx 2
+            addx 5
+            addx 2
+            addx 23
+            noop
+            addx -18
+            addx 1
+            noop
+            noop
+            addx 2
+            noop
+            noop
+            addx 7
+            addx 3
+            noop
+            addx 2
+            addx -27
+            addx 28
+            addx 5
+            addx -11
+            addx -27
+            noop
+            noop
+            addx 3
+            addx 2
+            addx 5
+            addx 2
+            addx 27
+            addx -26
+            addx 2
+            addx 5
+            addx 2
+            addx 4
+            addx -3
+            addx 2
+            addx 5
+            addx 2
+            addx 3
+            addx -2
+            addx 2
+            noop
+            addx -33
+            noop
+            noop
+            noop
+            noop
+            addx 31
+            addx -26
+            addx 6
+            noop
+            noop
+            addx -1
+            noop
+            addx 3
+            addx 5
+            addx 3
+            noop
+            addx -1
+            addx 5
+            addx 1
+            addx -12
+            addx 17
+            addx -1
+            addx 5
+            noop
+            noop
+            addx 1
+            noop
+            noop""";
 }
